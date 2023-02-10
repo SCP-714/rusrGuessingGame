f876d20b5e600dcf812c7bba72243751aa6dca78
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-09 21:35:32 -0600

    added comments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,12 +4,13 @@ fn main() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
-
+   //adds mutable variable for user input
     let mut guess = String::new();
 
+    //error handeling
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
+    //prints user inputted guess
     println!("You guessed: {guess}");
 }
