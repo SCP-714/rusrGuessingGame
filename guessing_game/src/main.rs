52dfbdacaa0631c6f459d84333939af7f570df84
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-10 09:23:49 -0600

    added string conversion

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -17,9 +17,12 @@ fn main() {
     io::stdin()
     .read_line(&mut guess)
     .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     //prints user inputted guess
     println!("You guessed: {guess}");
-    
+
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
