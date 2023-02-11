5eee110c288b81eaffbc23d148bbbb92557aee84
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-11 17:48:53 -0600

    added loop to continue game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
+    loop {
+
     println!("Please input your guess.");
     
     //adds mutable variable for user input
@@ -26,6 +28,10 @@ fn main() {
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+            }
+        }
     }
 }
