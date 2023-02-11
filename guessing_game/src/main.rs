f2ef9ffdc46b1f17723a4b9c3839aeb965bd58bb
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-11 17:52:01 -0600

    game fix on non num

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -20,7 +20,12 @@ fn main() {
     .read_line(&mut guess)
     .expect("Failed to read line");
 //converts str to num
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+let guess: u32 = match guess.trim().parse() {
+    Ok(num) => num,
+    Err(_) => continue,
+};
+
+println!("You guessed: {guess}");
 
     //prints user inputted guess
     println!("You guessed: {guess}");
